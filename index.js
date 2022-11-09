b8e07a2fc147bca767e67cc4eb605963522cc3d1
Arman <hurianarmann@gmail.com>
2022-11-09 13:28:46 +0330

    Add Day1

---
+++ index.js
@@ -0,0 +1,11 @@
+let myTitle = document.getElementById('title')
+
+let mylist = document.getElementsByClassName('mydev')
+
+let ullist =document.getElementsByTagName('li')
+
+let firstlist =document.querySelector('.mydev')
+
+let list =document.querySelectorAll('.mydev')
+
+console.log(list)
