53233b6de2f9080055738293e0999be9ef607d7f
Arman <hurianarmann@gmail.com>
2022-11-16 01:53:43 +0330

    Add Day 8

--- index.js
+++ index.js
@@ -127,3 +127,19 @@ function checkLogin(){
     }
 }
 
+let redValue , greenValue , blueValue
+
+setInterval(function(){
+    redValue = Math.floor(Math.random()*255)
+    greenValue = Math.floor(Math.random()*255)
+    blueValue = Math.floor(Math.random()*255)
+
+    document.body.style.backgroundColor='rgb('+ redValue +','+ greenValue + ','+ blueValue +')' 
+},2000)
+
+
+function onChangeContries(){
+
+   console.log(selectBox.value) 
+}
+
